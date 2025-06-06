9b7167628e6ff575ac5d9cb0266ce8774c3e0507
Luiz Motta <luizjoaomotta@gmail.com>
2025-06-06 14:01:09 -0300

    Add missing SpringBootApplication file and ILMT

---
+++ src/main/java/com/acme/BamoeSpringBootApplication.java
@@ -0,0 +1,11 @@
+package com.acme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = { "com.acme.**", "http**" })
+public class BamoeSpringBootApplication {
+    public static void main(String[] args) {
+       SpringApplication.run(BamoeSpringBootApplication.class, args);
+    }
+}
