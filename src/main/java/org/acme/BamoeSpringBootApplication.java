21ce53ff61b2269168d3a601404e8c26e25397dd
Thiago Lugli <thiagoelg@gmail.com>
2025-09-11 18:03:22 -0300

    9.3.0-ibm-0004-spring-boot-dmn

--- src/main/java/org/acme/BamoeSpringBootApplication.java
+++ src/main/java/org/acme/BamoeSpringBootApplication.java
@@ -1,9 +1,10 @@
-package com.acme;
+// Copyright IBM Corp. 2025.
+package org.acme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = { "org.kie.kogito.dmn.**", "org.kie.kogito.app.**", "com.acme.**", "http**" })
+@SpringBootApplication(scanBasePackages = { "org.acme.**", "http**" })
 public class BamoeSpringBootApplication {
     public static void main(String[] args) {
        SpringApplication.run(BamoeSpringBootApplication.class, args);
