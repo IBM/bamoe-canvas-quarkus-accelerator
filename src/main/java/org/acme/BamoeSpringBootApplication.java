84d37ff6945ae1781b50f49d8dfbdeb69b715ae4
Jijo Thomas K <Jijo.Thomas.K@ibm.com>
2025-11-11 18:29:03 +0530

    9.3.1-ibm-0001-spring-boot-full

--- src/main/java/org/acme/BamoeSpringBootApplication.java
+++ src/main/java/org/acme/BamoeSpringBootApplication.java
@@ -4,7 +4,7 @@ package org.acme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = { "org.acme.**", "http**" })
+@SpringBootApplication(scanBasePackages = { "org.kie.**", "org.acme.**", "org.drools.**", "org.jbpm.**" })
 public class BamoeSpringBootApplication {
     public static void main(String[] args) {
        SpringApplication.run(BamoeSpringBootApplication.class, args);
