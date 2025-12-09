dca04a88a0c3ea47d464465baef59bc807d323ab
BAMOE CI <bamoe.ci@ibm.com>
2025-12-09 07:05:15 +0000

    Update accelerators for 9.3.1-ibm-0006-spring-boot-dmn

--- src/main/java/org/acme/BamoeSpringBootApplication.java
+++ src/main/java/org/acme/BamoeSpringBootApplication.java
@@ -8,7 +8,7 @@ package org.acme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = { "org.acme.**", "http**" })
+@SpringBootApplication(scanBasePackages = { "org.kie.**", "org.acme.**", "org.drools.**", "org.jbpm.**", "com.ibm.bamoe.**", "http**" })
 public class BamoeSpringBootApplication {
     public static void main(String[] args) {
        SpringApplication.run(BamoeSpringBootApplication.class, args);
